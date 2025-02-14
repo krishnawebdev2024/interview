e660f4019d7f928c03af00c1bd27509e41958ec2
krishna <krisch.webdev@gmail.com>
2025-02-15 00:12:03 +0100

    initial_commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-slate-800 text-white p-2">App</div>
+      <button onClick={() => alert("Hello, Vite!")}>Click Me</button>
+    </>
+  );
+};
+
+export default App;
